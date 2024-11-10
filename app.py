657845a6b6a6f2bc68b2e69d4b939bf6d2fe2b6c
Hero <jacobtaylor048@gmail.com>
2024-11-10 16:35:23 -0500

    Add /home route to website

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route("/home")
 def homepage():
     return render_template("homepage.html")
+
+
+
     
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
