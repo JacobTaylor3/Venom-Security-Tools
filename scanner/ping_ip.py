2ed020fb65e3037f88ededd36a08d20564ba54a4
Hero <jacobtaylor048@gmail.com>
2024-11-10 18:34:34 -0500

    Finalized ping framework

--- scanner/ping_ip.py
+++ scanner/ping_ip.py
@@ -7,19 +7,28 @@ from typing import Optional
 
 
 
+
 def checkDataPacket(response:tuple[Optional[Packet], float])->str:
-    if response[0] is None:
-        return "No Response"
+    if response[0] is not None and response[0].haslayer(ICMP):
+
+       PACKET = response[0][IP]
+       
+       src = PACKET.src
+       time = response[1]
+       ttl = PACKET.ttl
+       
+       return f"Reply from: {src}  time: {time} ms  TTL: {ttl}"
     
     else:
-       return f"Response:{response[0].show()}"
+        return "Destination unreachable"
+       
     
 
 def pingIpAdr(ip:str,timeout_pr =8)-> tuple[Optional[Packet], float]: #test this function for testing make sure its not None
        
      startTime = time.time() #bytes
     
-     response = sr1(IP(dst= ip,timeout= timeout_pr) /ICMP(),verbose= False)  
+     response = sr1(IP(dst= ip) /ICMP(),verbose= False)  
     
      endTime = time.time()
     
@@ -31,6 +40,11 @@ def pingIpAdr(ip:str,timeout_pr =8)-> tuple[Optional[Packet], float]: #test this
    
    
 def sendPing(ipAdr:str):
-    
-    return checkDataPacket(pingIpAdr(ipAdr))
+    output = []
+    for _i in range(0,3):
+        output.append(checkDataPacket(pingIpAdr(ipAdr)))
+     
+    return output
 
+
+print(sendPing("8.8.8.8"))
